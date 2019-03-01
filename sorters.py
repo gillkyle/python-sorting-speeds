7975f0cade4ebcafea612e7ea883868bc4832c9f
gillkyle <kylerobertgill@gmail.com>
2019-02-28 22:00:06 -0700

    linting fixes

--- sorters.py
+++ sorters.py
@@ -18,20 +18,54 @@
 # (null data list, index out of bounds, etc.)
 #
 
+
 def bubble_sort(data, index, descending=False):
     '''Sorts using the bubble sort algorithm'''
-    data.sort(key=lambda t: t[index], reverse=descending)
+    for end_cursor in range(len(data) - 1, 0, -1):
+        for idx in range(end_cursor):
+            if descending and (data[idx][index] < data[idx + 1][index]):
+                data[idx], data[idx + 1] = data[idx + 1], data[idx]
+            elif not descending and (data[idx][index] > data[idx + 1][index]):
+                data[idx], data[idx + 1] = data[idx + 1], data[idx]
+    return data
+
 
 def insertion_sort(data, index, descending=False):
     '''Sorts using the insertion sort algorithm'''
-    # replace this with your own algorithm (do not use Python's sort)
-    data.sort(key=lambda t: t[index], reverse=descending)
+    for cursor in range(1, len(data)):
+        current = data[cursor]
+        position = cursor
+
+        if descending:
+            while position > 0 and data[position - 1][index] < current[index]:
+                data[position] = data[position - 1]
+                position -= 1
+        else:
+            while position > 0 and data[position - 1][index] > current[index]:
+                data[position] = data[position - 1]
+                position -= 1
+
+        data[position] = current
+    return data
 
 
 def selection_sort(data, index, descending=False):
     '''Sorts using the selection sort algorithm'''
-    # replace this with your own algorithm (do not use Python's sort)
-    data.sort(key=lambda t: t[index], reverse=descending)
+    for cursor in range(len(data) - 1, 0, -1):
+        target_index = 0
+
+        if descending:
+            for position in range(1, cursor + 1):
+                if data[position][index] < data[target_index][index]:
+                    target_index = position
+
+        else:
+            for position in range(1, cursor + 1):
+                if data[position][index] > data[target_index][index]:
+                    target_index = position
+
+        data[cursor], data[target_index] = data[target_index], data[cursor]
+    return data
 
 
 def quick_sort(data, index, descending=False):
